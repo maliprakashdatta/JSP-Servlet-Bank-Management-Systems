06ccfec7ae72e4ffa7ae64fc20807e0abc678a2f
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-09-03 19:05:09 +0530

    committed

--- src/main/java/com/swsa/service/CheckBalanceOperationService.java
+++ src/main/java/com/swsa/service/CheckBalanceOperationService.java
@@ -1,18 +1,12 @@
 package com.swsa.service;
-import com.swsa.model.Account;
 import com.swsa.repository.AccountRepository;
-import java.sql.SQLException;
-import java.util.List;
 
-public class CheckBalanceOperationService {
+public class CheckBalanceOperationService
+{
 
         private static final AccountRepository ACCOUNT_REPOSITORY = new AccountRepository();
 
         public double checkBalance(String accountNumber) {
-            return ACCOUNT_REPOSITORY.findBalanceByAccountNumber(accountNumber);
+            return ACCOUNT_REPOSITORY.checkBalance(accountNumber);
         }
-
-   public List<Account> retrieveAccount() throws SQLException {
-       return ACCOUNT_REPOSITORY.retrieveAccount();
-   }
 }
